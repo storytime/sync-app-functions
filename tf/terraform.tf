b0f570406d78e83532e75e0557043d7d4be2c0c7
Bo <storytime@users.noreply.github.com>
2023-01-11 16:13:28 +0200

    -- refactoring

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -1,17 +1,22 @@
 terraform {
-
-  cloud {
-    organization = "sync-app"
-
-    workspaces {
-      name = "sync-app-functions"
-    }
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.48.0"
     }
   }
+}
+
+terraform {
+  backend "s3" {
+
+    bucket = "sync-app-tf-state"
+    key    = "functions-state/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  profile = var.aws_profile
+  region  = var.aws_region
 }
