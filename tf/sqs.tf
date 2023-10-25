297014ec1549c49e2f38079402be816258cff11d
Bo <storytime@users.noreply.github.com>
2023-10-25 11:36:33 +0300

    -- change sqs message_retention period

--- tf/sqs.tf
+++ tf/sqs.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "export-be-queue" {
   name                       = data.aws_ssm_parameter.export_be_queue.value
   delay_seconds              = 0
   max_message_size           = 1024
-  message_retention_seconds  = 10800
+  message_retention_seconds  = 90
   visibility_timeout_seconds = 90
 }
 
@@ -11,6 +11,6 @@ resource "aws_sqs_queue" "backup-queue" {
   name                       = data.aws_ssm_parameter.backup_starter_queue.value
   delay_seconds              = 0
   max_message_size           = 1024
-  message_retention_seconds  = 10800
+  message_retention_seconds  = 90
   visibility_timeout_seconds = 90
 }
